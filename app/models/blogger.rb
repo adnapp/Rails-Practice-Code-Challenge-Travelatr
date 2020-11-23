0422df64e3fd7b40aad40f0ebca76971dfd985d0
adnapp <adam.napps@gmail.com>
2020-11-23 17:10:18 -0500

    draft

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,20 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates :name, uniqueness: true
+    validates :age, numericality: { greater_than_or_equal_to: 0,  only_integer: true }
+    validates :bio, length: { minimum: 30}
+    
+    
+    def number_likes
+        self.posts.sum do |post|
+            post.likes 
+        end
+    end
 
+    def featured_post
+        self.posts.max_by do |post|
+            post.likes
+        end
+    end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,15 @@
 class Destination < ApplicationRecord
+    has_many :posts 
+    has_many :bloggers, through: :posts
+    
+    def featured_post
+        self.posts.max_by do |post|
+            post.likes
+        end
+    end
 
+    def blogger_average_age
+        sum = self.bloggers.sum {|b| b.age}
+        sum/self.bloggers.count
+    end
 end
