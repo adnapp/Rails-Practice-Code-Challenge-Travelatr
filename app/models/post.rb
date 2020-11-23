c62850267deebd4ab4514bd74b5974491862ac1b
adnapp <adam.napps@gmail.com>
2020-11-23 17:34:02 -0500

    final group cc

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
     belongs_to :destination
     validates :title, presence: :true
     validates :content,  length: { minimum: 100 }
+    
+    def add_like
+        self.likes += 1
+    end
+    
 end
